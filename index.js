f7287bc45a129a40a384efb2270ca5af6cb9aeb5
123pstewart <71267908+123pstewart@users.noreply.github.com>
2021-10-28 10:05:29 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// Use getElementById to get the two numbers and store them in variables
+
+// Create a function called multiplyNumbers
+// In the function use getElementById to change the ? to the answer of the equation
+
+// If you get that working, create new forms for the other arithmetic operators
